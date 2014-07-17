2288d4bc2fbb43c9aa5fe1d993090e357a6ff560
Michael Stark <stark3@gmail.com>
2014-07-17 14:28:14 -0400

    Honor using bot.memory, heretics one vote per user

--- honor.py
+++ honor.py
@@ -2,6 +2,14 @@ import hashlib
 
 import willie
 
+def setup(bot):
+    if not bot.memory.contains('honor'):
+        bot.memory['honor'] = willie.tools.WillieMemory()
+        bot.memory['honor'] = [ 'bacon', 'sweet tea', 'worfbot', 'god', 'mogh', 'gimli', 'moghbot', 'william riker', 'mstark', 'namer98', 'i can', 'aragorn, son of arathorn', 'captain picard', 'jean luc picard', 'jesus', 'gimli, son of gloin', 'blood wine', 'worf', 'commander riker', 'sweetened iced tea', 'st. thomas aquinas', 'aragorn', 'jesus christ' ]
+    if not bot.memory.contains('dishonor'):
+        bot.memory['dishonor'] = willie.tools.WillieMemory()
+        bot.memory['dishonor'] = [ 'eclipse', 'wasp', 'romulans', 'wasps', 'optimum', 'cheating', 'dishonor', 'lucifer', 'satan', 'deanna troi', 'quark', 'house of durasbot', 'house of duras', 'duras', 'com', 'lwaxana troi', 'durasbot' ]
+
 @willie.module.commands('honor')
 @willie.module.example('.honor')
 def honor(bot, trigger):
@@ -12,12 +20,9 @@ def honor(bot, trigger):
     say_honor(bot, topic.lower().strip())
 
 def say_honor(bot, topic):
-    topics_honorable = [ 'bacon', 'sweet tea', 'worfbot', 'god', 'mogh', 'gimli', 'moghbot', 'william riker', 'mstark', 'namer98', 'i can', 'aragorn, son of arathorn', 'captain picard', 'jean luc picard', 'jesus', 'gimli, son of gloin', 'blood wine', 'worf', 'commander riker', 'sweetened iced tea', 'st. thomas aquinas', 'aragorn', 'jesus christ' ]
-    topics_dishonorable = [ 'eclipse', 'wasp', 'romulans', 'wasps', 'optimum', 'cheating', 'dishonor', 'lucifer', 'satan', 'deanna troi', 'quark', 'house of durasbot', 'house of duras', 'duras', 'com', 'lwaxana troi', 'durasbot' ]
-
-    if topic in topics_honorable:
+    if topic in bot.memory['honor']:
         bot.say(topic + ' has honor')
-    elif topic in topics_dishonorable:
+    elif topic in bot.memory['dishonor']:
         bot.say(topic + ' is without honor')
     else:
         hasher = hashlib.md5()
