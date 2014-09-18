a6dd7394301d18fd7a4643da473cc23f005b8ce5
Michael Stark <stark3@gmail.com>
2014-09-18 14:17:18 -0400

    Add satoshi price as well.

--- doge.py
+++ doge.py
@@ -8,4 +8,6 @@ def bible(bot, trigger):
     '''Look up the current prices from chain.so and average them.'''
     resp = json.loads(requests.get('https://chain.so/api/v2/get_price/DOGE/USD').text)
     prices = [float(p['price']) for p in resp['data']['prices']]
-    return bot.say('Dogecoin price: ' + str(sum(prices) / len(prices)) + ' USD')
+    btcresp = json.loads(requests.get('https://chain.so/api/v2/get_price/DOGE/BTC').text)
+    btcprices = [float(p['price']) for p in btcresp['data']['prices']]
+    return bot.say('Dogecoin price: ' + str(sum(prices) / len(prices)) + ' USD, ' + str(100000000 * sum(btcprices) / len(btcprices)) + ' Sat')
