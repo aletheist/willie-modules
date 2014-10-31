f6e7491849a2f8718617548520344960970130dc
Joseph Dunn <joseph@tx-0.org>
2014-10-30 21:55:11 -0500

    Country module

---
+++ country.py
@@ -0,0 +1,58 @@
+import re
+import datetime
+import dns.resolver
+
+import willie
+import requests
+
+def setup(bot):
+    if not bot.memory.contains('country_zz_zone'):
+        bot.memory['country_zz_zone'] = willie.tools.WillieMemory()
+        zz_text = requests.get('http://countries.nerd.dk/isolist.txt').text
+        for match in re.findall('([a-z]{2})\s+(\d+\.\d+\.\d+\.\d+)', zz_text):
+            bot.memory['country_zz_zone'][match[1]] = match[0]
+    if not bot.memory.contains('country_iso_3166'):
+        bot.memory['country_iso_3166'] = willie.tools.WillieMemory()
+        iso_text = requests.get('http://www.combit.net/de/support/files/cmbtkb/country_names_and_code_elements.txt').text.encode('utf-8')
+        for match in re.findall('(.+?);([A-Z]{2})', iso_text):
+            if match[0] != 'Country Name':
+                bot.memory['country_iso_3166'][match[1].lower()] = match[0]
+    if not bot.memory.contains('country_ip_cache'):
+        bot.memory['country_ip_cache'] = willie.tools.WillieMemory()
+    if not bot.memory.contains('country_host_timestamp'):
+        bot.memory['country_host_timestamp'] = willie.tools.WillieMemory()
+
+@willie.module.event('JOIN')
+@willie.module.rule('.*')
+def country(bot, trigger):
+    if trigger.host in bot.memory['country_host_timestamp'] and (datetime.datetime.now() - bot.memory['country_host_timestamp'][trigger.host]).total_seconds() < 30:
+        return
+    else:
+        bot.memory['country_host_timestamp'][trigger.host] = datetime.datetime.now()
+
+    ip = None
+    m = re.search('ip.(\d+\.\d+\.\d+\.\d+)$', trigger.host)
+    if m is None:
+        try:
+            # have to look up IP address for hostname
+            ip = dns.resolver.query(trigger.host, 'A')[0].address
+        except dns.resolver.NXDOMAIN:
+            pass
+    else:
+        ip = m.group(1)
+
+    country = 'an undisclosed location'
+    if ip is not None:
+        if ip in bot.memory['country_ip_cache']:
+            country = bot.memory['country_ip_cache'][ip]
+        else:
+            zz_ip = dns.resolver.query(reverse_ip(ip) + '.zz.countries.nerd.dk', 'A')[0].address
+            if zz_ip in bot.memory['country_zz_zone']:
+                country_code = bot.memory['country_zz_zone'][zz_ip]
+                if country_code in bot.memory['country_iso_3166']:
+                    country = bot.memory['country_iso_3166'][country_code]
+                    bot.memory['country_ip_cache'][ip] = country
+    bot.say('A wild ' + trigger.nick + ' arrives from ' + country + '!')
+
+def reverse_ip(ip):
+    return '.'.join(ip.split('.')[::-1])
