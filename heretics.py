ce9100e031c9137bc5b0bac54db9b921bd4e52e6
Mike Stark <mstark@factset.com>
2015-08-21 15:33:41 -0400

    Fix typos and functions
    Top X Heretics still broken.

--- heretics.py
+++ heretics.py
@@ -11,7 +11,7 @@ def denounce_heretic(bot, trigger):
 
     # Initialize the heretic
     denounce_key = 'denounce_%s' % str(target)
-    defense_hey = 'defense_%s' % str(target)
+    defense_key = 'defense_%s' % str(target)
     denounce_history = bot.db.get_channel_value(channel, denounce_key)
     defense_history = bot.db.get_channel_value(channel, defense_key)
     if denounce_history is None or defense_history is None:
@@ -37,7 +37,7 @@ def deny_heresy(bot, trigger):
 
     # Initialize the heretic
     denounce_key = 'denounce_%s' % str(target)
-    defense_hey = 'defense_%s' % str(target)
+    defense_key = 'defense_%s' % str(target)
     denounce_history = bot.db.get_channel_value(channel, denounce_key)
     defense_history = bot.db.get_channel_value(channel, defense_key)
     if denounce_history is None or defense_history is None:
@@ -56,7 +56,7 @@ def deny_heresy(bot, trigger):
 
 def set_heretic_values(bot, target, channel, denunciations, defenses):
     denounce_key = 'denounce_%s' % str(target)
-    defense_hey = 'defense_%s' % str(target)
+    defense_key = 'defense_%s' % str(target)
     bot.db.set_channel_value(channel, denounce_key, denunciations)
     bot.db.set_channel_value(channel, defense_key, defenses)
 
@@ -67,14 +67,14 @@ def set_heretic_values(bot, target, channel, denunciations, defenses):
         all_heretics.append(target)
     bot.db.set_channel_value(channel, 'heretics', all_heretics)
 
-def score(bot, target):
+def score(bot, channel, target):
     denounce_key = 'denounce_%s' % str(target)
     denounce_history = bot.db.get_channel_value(channel, denounce_key)
     heretic_count = 0
     if denounce_history is not None:
         heretic_count = len(denounce_history)
 
-    defense_hey = 'defense_%s' % str(target)
+    defense_key = 'defense_%s' % str(target)
     defense_history = bot.db.get_channel_value(channel, defense_key)
     defense_count = 0
     if defense_history is not None:
@@ -87,13 +87,14 @@ def score(bot, target):
 def heretics(bot, trigger):
     '''Lists the top X known heretics.'''
     num = 5
+    channel = trigger.sender
     try:
         num = min(int(trigger.group(2)), 10) # Max out at 10 heretics to avoid spam (*ahem* ALETHEIST *ahem*)
     except:
         pass
     bot.say('Top %d Heretics' % num)
     all_heretics = bot.db.get_channel_value(channel, 'heretics')
-    for i, heretic in enumerate([ x for x in sorted(map(functools.partial(score, bot=bot), all_heretics), key=operator.itemgetter(1), reverse=True) if x[1] > 0][:num]):
+    for i, heretic in enumerate([ x for x in sorted(map(functools.partial(score, bot=bot, channel=channel), all_heretics), key=operator.itemgetter(1), reverse=True) if x[1] > 0][:num]):
         bot.say('  #' + str(i + 1) + ' ' + heretic[0] + ' (' + str(heretic[1]) + ' denunciation' + ('s' if heretic[1] != 1 else '') + ')')
 
 @sopel.module.commands('heretic')
@@ -101,10 +102,11 @@ def heretics(bot, trigger):
 def heretic(bot, trigger):
     '''Shows the "heretic score" of the given target, or the user if no target is given.'''
     target = trigger.nick
+    channel = trigger.sender
     if trigger.group(2):
         target = trigger.group(2)
 
-    total = score(bot, target)[1]
+    total = score(bot, channel, target)[1]
     bot.say(target + ' (' + str(total) + ' denunciation' + ('s' if total != 1 else '') + ')')
 
 @sopel.module.commands('denunciations', 'denounced', 'mh', 'myheretics', 'defenses', 'defended')
@@ -118,10 +120,10 @@ def denounced(bot, trigger):
     defended = [t for t in all_heretics if nick in bot.db.get_channel_value(channel, 'defense_%s' % str(t))]
     report = ''
     if len(denounced) == 0:
-        report = report + username + ' has not denounced anything'
+        report = report + nick + ' has not denounced anything'
     else:
         string = ", ".join(denounced)
-        report = report + username + ' has denounced: ' + string
+        report = report + nick + ' has denounced: ' + string
     if len(defended) == 0:
         report = report + ', and has not defended anything.'
     else:
