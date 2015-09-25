75824be40d94652885dc8c554262d38496358892
Joseph Dunn <joseph@tx-0.org>
2015-09-25 16:04:22 -0500

    Start sending .denom replies as private messages to peopel who are querying a lot

--- denom.py
+++ denom.py
@@ -1,9 +1,17 @@
 import random
+import datetime
 import sopel
 
 def setup(bot):
+    if not bot.memory.contains('denom_nick_last_query'):
+        bot.memory['denom_nick_last_query'] = sopel.tools.SopelMemory()
+    if not bot.memory.contains('denom_nick_fast_query_count'):
+        bot.memory['denom_nick_fast_query_count'] = sopel.tools.SopelMemory()
+    if not bot.memory.contains('denom_nick_reply_via_message'):
+        bot.memory['denom_nick_reply_via_message'] = sopel.tools.SopelMemory()
     if not bot.memory.contains('denom_ns_pending_checks'):
         bot.memory['denom_ns_pending_checks'] = sopel.tools.SopelMemory()
+    if not bot.memory.contains('denom_ns_checking'):
         bot.memory['denom_ns_checking'] = None
 
 @sopel.module.commands('setdenom', 'mydenom')
@@ -31,10 +39,42 @@ def get_denom(bot, trigger):
   if trigger.group(2):
     person = trigger.group(2)
 
-  if bot.db.get_nick_value(person,'denom'):
-    bot.reply('%s is %s' % (person, bot.db.get_nick_value(person.lower(),'denom')))
+  reply_via_msg = False
+
+  if trigger.nick != trigger.sender:
+    now = datetime.datetime.now()
+    if person in bot.memory['denom_nick_last_query']:
+        elapsed_since_last = (now - bot.memory['denom_nick_last_query'][person]).total_seconds()
+        if elapsed_since_last < 10:
+            if person in bot.memory['denom_nick_fast_query_count']:
+                bot.memory['denom_nick_fast_query_count'][person] += 1
+            else:
+                bot.memory['denom_nick_fast_query_count'][person] = 1
+        else:
+            # reset
+            if person in bot.memory['denom_nick_fast_query_count']:
+                bot.memory['denom_nick_fast_query_count'][person] = 0
+            if person in bot.memory['denom_nick_reply_via_message'] and bot.memory['denom_nick_reply_via_message'][person] is True:
+                bot.memory['denom_nick_reply_via_message'][person] = False
+    bot.memory['denom_nick_last_query'][person] = now
+
+    if person in bot.memory['denom_nick_fast_query_count']:
+        if bot.memory['denom_nick_fast_query_count'][person] > 2:
+            if person not in bot.memory['denom_nick_reply_via_message'] or bot.memory['denom_nick_reply_via_message'][person] is False:
+                bot.memory['denom_nick_reply_via_message'][person] = True
+                bot.reply('Sending further replies as private messages.')
+            reply_via_msg = True
+
+  if reply_via_msg:
+    if bot.db.get_nick_value(person,'denom'):
+      bot.msg(person, '%s is %s' % (person, bot.db.get_nick_value(person.lower(),'denom')))
+    else:
+      bot.msg(person, 'I don\'t know what %s is.' % person)
   else:
-    bot.reply('I don\'t know what %s is.' % person)
+    if bot.db.get_nick_value(person,'denom'):
+      bot.reply('%s is %s' % (person, bot.db.get_nick_value(person.lower(),'denom')))
+    else:
+      bot.reply('I don\'t know what %s is.' % person)
 
 @sopel.module.event('NOTICE')
 @sopel.module.rule('Information on \x02(.*)\x02 \(account \x02(.*)\x02\):')
