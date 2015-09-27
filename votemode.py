f6aaf4800679d0d0a84159abb3ebd7bf04abcc7f
aletheist <aletheist@users.noreply.github.com>
2015-09-26 21:58:03 -0400

    Moved to a counting system that only lets users vote once.

--- votemode.py
+++ votemode.py
@@ -1,5 +1,7 @@
 import sopel
 from sopel.module import commands, priority, OP, HALFOP, VOICE, require_privilege
+from sopel.tools import Identifier
+from enum import Enum
 from math import ceil
 from datetime import datetime
 from datetime import timedelta
@@ -11,12 +13,19 @@ def setup(bot):
   bot.memory['last_voteban'] = datetime.now()
   bot.memory['kick_votes'] = dict()
 
+class Modes(Enum):
+    Kick = .5
+    Ban = .66
+    Voice = .25
+    Moderated = .3
+    RegisteredOnly = .4
+
 def prune_active_users(bot):
   for c in bot.memory['active_users']:
     to_prune = list()
     for u in bot.memory['active_users'][c]:
       print('DEBUG: Removing %s' % bot.memory['active_users'][c][u])
-      if bot.memory['active_users'][c][u] + timedelta(seconds=5) < datetime.now():
+      if bot.memory['active_users'][c][u] + timedelta(minutes=5) < datetime.now():
         to_prune.append(u)
     for u in to_prune:
       del bot.memory['active_users'][c][u]
@@ -43,34 +52,49 @@ def show_active_users(bot, trigger):
   for u in bot.memory['active_users'][channel]:
     bot.reply("%s" % u)
 
+def calculate_quota(bot, trigger, threshold):
+  channel = trigger.sender
+  quota = (ceil(len(bot.memory['active_users'][channel])*threshold))
+  return quota
+
+#def votemode(bot, mode):
+
 @require_privilege(VOICE)
 @sopel.module.commands('votekick')
 def votekick(bot, trigger):
+  make_user_active(bot, trigger)
   channel = trigger.sender
   nick = trigger.nick
-  quota = (ceil(len(bot.memory['active_users'][channel])/2))
+  quota = calculate_quota(bot, trigger, 0.5) 
+  prune_active_users(bot)
   # This isn't per user but it's probably an OK heuristic
   if datetime.now() - bot.memory['last_votekick'] > timedelta(minutes=5):
     bot.memory['kick_votes'] = dict()
   # Quota is 50% of active users plus one
   if trigger.group(2):
     target = str(trigger.group(2)).strip()
+    if not Identifier(target).is_nick():
+      bot.reply("That is not a valid nick")
+      return
     target_privs = bot.privileges[channel][target]
     if target_privs > 0:
      bot.reply("You cannot votekick privileged users")
      return
     
     if target in bot.memory['kick_votes']:
-      bot.memory['kick_votes'][target] = bot.memory['kick_votes'][target] + 1
+      if str(nick) not in bot.memory['kick_votes'][target]:
+        bot.memory['kick_votes'][target].append(str(nick))
     else:
-      bot.memory['kick_votes'][target] = 1
+      bot.memory['kick_votes'][target] = list()
+      bot.memory['kick_votes'][target].append(str(nick))
+    bot.reply("Vote recorded.")
     
-    if bot.memory['kick_votes'][target] > quota:
+    if len(bot.memory['kick_votes'][target]) > quota:
       bot.write(['KICK', channel, target], "You have been voted off the island.")
     bot.memory['last_votekick'] = datetime.now()
   else:
-    bot.reply("Current active votekicks (%s needed to kick): " % str(quota))
+    bot.say("Current active votekicks (%s needed to kick): " % str(quota + 1))
     for ballot in bot.memory['kick_votes']:
-      bot.reply("%s has %s kick votes." % (ballot, bot.memory['kick_votes'][ballot]))
+      bot.say("%s has %s kick votes." % (ballot, len(bot.memory['kick_votes'][ballot])))
     return
 
