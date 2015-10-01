19bd518661868590d61491874ad8ed24e047c120
aletheist <aletheist@users.noreply.github.com>
2015-09-30 20:20:00 -0400

    Completed voteregistered and votemoderated

--- votemode.py
+++ votemode.py
@@ -1,5 +1,5 @@
 import sopel
-from sopel.module import commands, priority, OP, HALFOP, VOICE, require_privilege
+from sopel.module import OP, VOICE, require_privilege
 from sopel.tools import Identifier
 from math import floor
 from datetime import datetime
@@ -10,9 +10,10 @@ def clear_votes(bot):
     bot.memory['votes'] = {'kick': dict(),
                            'ban': dict(),
                            'voice': dict(),
-                           'registered': dict(),
-                           'moderated': dict()
+                           'registered': list(),
+                           'moderated': list()
                           }
+
 def setup(bot):
     bot.memory['active_users'] = dict()
     bot.memory['last_vote'] = datetime.now()
@@ -22,12 +23,13 @@ def setup(bot):
     bot.memory['mode_threshold']['moderated'] = 0.3
     bot.memory['mode_threshold']['registered'] = 0.4
     bot.memory['mode_threshold']['voice'] = 0.4
-    bot.memory['vote_methods'] = { 'kick': do_kick,
-                                   'ban': do_ban,
-                                   'voice': do_voice,
-                                   'registered': do_registered,
-                                   'moderated': do_moderated
+    bot.memory['vote_methods'] = {'kick': do_kick,
+                                  'ban': do_ban,
+                                  'voice': do_voice,
+                                  'registered': do_registered,
+                                  'moderated': do_moderated
                                  }
+    bot.memory['protected_timestamp'] = None
     clear_votes(bot)
 
 def prune_active_users(bot):
@@ -39,6 +41,14 @@ def prune_active_users(bot):
         for user in to_prune:
             del bot.memory['active_users'][channel][user]
 
+def clear_protection(bot, channel):
+    if bot.memory['protected_timestamp'] is None:
+        return
+    if bot.memory['protected_timestamp'] + timedelta(minutes=30) < datetime.now():
+        bot.memory['protected_timestamp'] = None
+        bot.write(['mode', channel, '-r'])
+        bot.write(['mode', channel, '-m'])
+
 @require_privilege(VOICE)
 @sopel.module.rule(r'.*')
 @sopel.module.priority('low')
@@ -48,13 +58,13 @@ def make_user_active(bot, trigger):
     if channel not in bot.memory['active_users']:
         bot.memory['active_users'][channel] = dict()
     bot.memory['active_users'][channel][nick] = datetime.now()
+    clear_protection(bot, channel)
 
 @sopel.module.commands('activeusers')
 @sopel.module.example('.activeusers')
 def show_active_users(bot, trigger):
     make_user_active(bot, trigger)
     channel = trigger.sender
-    nick = trigger.nick
     if channel not in bot.memory['active_users']:
         bot.say("No active users")
         return
@@ -76,10 +86,12 @@ def do_voice(bot, channel, target):
     bot.write(['mode', channel, '+v', target])
 
 def do_registered(bot, channel):
-    pass
+    bot.write(['mode', channel, '+r'])
+    bot.memory['protected_timestamp'] = datetime.now()
 
 def do_moderated(bot, channel):
-    pass
+    bot.write(['mode', channel, '+m'])
+    bot.memory['protected_timestamp'] = datetime.now()
 
 def votemode(bot, trigger, mode):
     make_user_active(bot, trigger)
@@ -87,7 +99,7 @@ def votemode(bot, trigger, mode):
     nick = trigger.nick
     if bot.privileges[trigger.sender][bot.nick] < OP:
         return bot.reply("I'm not a channel operator!")
-    quota = calculate_quota(bot, trigger, bot.memory['mode_threshold'][mode]) 
+    quota = calculate_quota(bot, trigger, bot.memory['mode_threshold'][mode])
     prune_active_users(bot)
     # This isn't per user but it's probably an OK heuristic
     if datetime.now() - bot.memory['last_vote'] > timedelta(minutes=5):
@@ -102,17 +114,26 @@ def votemode(bot, trigger, mode):
         target_privs = bot.privileges[channel][target]
         if target_privs > 0:
             return bot.reply("You cannot vote" + mode + " privileged users")
-        
+
         if target in bot.memory['votes'][mode]:
             if str(nick) not in bot.memory['votes'][mode][target]:
                 bot.memory['votes'][mode][target].append(str(nick))
         else:
             bot.memory['votes'][mode][target] = [str(nick)]
-        
-        bot.reply("Vote recorded. (%s more votes for action)" % str(quota - len(bot.memory['votes'][mode][target])+1))
-        
+
+        bot.reply("Vote recorded. (%s more votes for action)" % str(max(0, quota - len(bot.memory['votes'][mode][target])+1)))
+
         if len(bot.memory['votes'][mode][target]) > quota:
             bot.memory['vote_methods'][mode](bot, channel, target)
+        bot.memory['last_vote'] = datetime.now()
+    elif mode == "registered" or mode == "moderated":
+        if str(nick) not in bot.memory['votes'][mode]:
+            bot.memory['votes'][mode].append(str(nick))
+        else:
+            bot.memory['votes'][mode] = [str(nick)]
+        bot.reply("Vote recorded. (%s more votes for action)" % str(max(0, quota - len(bot.memory['votes'][mode])+1)))
+        if len(bot.memory['votes'][mode]) > quota:
+            bot.memory['vote_methods'][mode](bot, channel)
         bot.memory['last_vote'] = datetime.now()
     else:
         bot.say("Current active vote%s (%s needed to %s): " % (mode, str(quota + 1), mode))
@@ -135,14 +156,29 @@ def votekick(bot, trigger):
 def votevoice(bot, trigger):
     votemode(bot, trigger, 'voice')
 
-@require_privilege(OP)
+@require_privilege(VOICE)
+@sopel.module.commands('voteregistered')
+def voteregistered(bot, trigger):
+    votemode(bot, trigger, 'registered')
+
+@require_privilege(VOICE)
+@sopel.module.commands('votemoderated')
+def votemoderated(bot, trigger):
+    votemode(bot, trigger, 'moderated')
+
+@require_privilege(VOICE)
 @sopel.module.commands('bunnies')
 def nuclear_option(bot, trigger):
     channel = trigger.sender
     nick = trigger.nick
     bot.say("Bunnies are wonderful creatures. I enjoy watching them frolic.")
     bot.action('watches the bunnies frolic.')
-    if nick != "aletheist":
+    if bot.privileges[channel][bot.nick] < OP:
+        return
+    elif bot.privileges[channel][nick] < OP:
+        return
+    safety = str(trigger.group(2)).strip()
+    if nick != "aletheist" or str(safety) != "!":
         return
     sleep(5)
     bot.say("Nuclear launch detected.")
