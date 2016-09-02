013a53befe325374892d482f66b9886fe922a407
aletheist <aletheist@users.noreply.github.com>
2016-09-02 19:08:30 -0400

    New denom module strategy

--- denom.py
+++ denom.py
@@ -1,18 +1,19 @@
 import random
 import datetime
 import sopel
+import threading
+
+nickserv_lock = threading.Lock()
 
 def setup(bot):
+    bot.cap_req('votemode', 'extended-join')
+    bot.cap_req('votemode', 'account-notify')
     if not bot.memory.contains('denom_nick_last_query'):
         bot.memory['denom_nick_last_query'] = sopel.tools.SopelMemory()
     if not bot.memory.contains('denom_nick_fast_query_count'):
         bot.memory['denom_nick_fast_query_count'] = sopel.tools.SopelMemory()
     if not bot.memory.contains('denom_nick_reply_via_message'):
         bot.memory['denom_nick_reply_via_message'] = sopel.tools.SopelMemory()
-    if not bot.memory.contains('denom_ns_pending_checks'):
-        bot.memory['denom_ns_pending_checks'] = sopel.tools.SopelMemory()
-    if not bot.memory.contains('denom_ns_checking'):
-        bot.memory['denom_ns_checking'] = None
 
 @sopel.module.commands('setdenom', 'mydenom')
 @sopel.module.example('.setdenom Lutheran')
@@ -20,17 +21,34 @@ def set_denom(bot, trigger):
   '''Set a user's denomination'''
   length_limit = 128
   person = str(trigger.nick).lower()
+  account = trigger.account
   sender = trigger.sender
   denom = 'Trout'
   if trigger.group(2):
     denom = trigger.group(2)
 
-  if len(denom) > length_limit:
+  if account is None:
+    bot.msg(sender, 'Sorry, you need to be authed to services to use this command.')
+  elif len(denom) > length_limit:
     bot.reply('Denomination name too long. (Limit %s characters)' % str(length_limit))
-    return
-
-  bot.memory['denom_ns_pending_checks'][person] = [ sender, denom ]
-  bot.msg('NickServ', 'INFO ' + person + ' ' + str(random.randint(0,100))) # random character means that WorfBot will never send '...' after repeating himself, which otherwise is possible because hardcoded into sopel's irc.py
+  else:
+    print(person,str(account))
+    try:
+      # First we try to unalias the nick
+      bot.db.unalias_nick(trigger.nick)
+      try:
+        # If we were able to unalias, now stick that nick on the new account
+        # Someone can steal a nick within the ghosting time limit unfortunately
+        bot.db.alias_nick(account, trigger.nick)
+      except ValueError as e:
+        pass
+    except ValueError as e:
+      # If it's not an alias, we merge instead
+      print(bot.db.get_nick_id(account), bot.db.get_nick_id(trigger.nick))
+      bot.db.merge_nick_groups(account, trigger.nick)
+      
+    bot.db.set_nick_value(account, 'denom', denom)
+    bot.msg(sender, 'Got it: %s is %s' % (person, denom))
 
 @sopel.module.commands('denom', 'getdenom')
 @sopel.module.example('.denom mstark')
@@ -67,52 +85,12 @@ def get_denom(bot, trigger):
 
   if reply_via_msg:
     if bot.db.get_nick_value(person,'denom'):
-      bot.msg(person, '%s is %s' % (person, bot.db.get_nick_value(person.lower(),'denom')))
+      bot.msg(person, '%s is %s' % (person, bot.db.get_nick_value(person,'denom')))
     else:
       bot.msg(person, 'I don\'t know what %s is.' % person)
   else:
     if bot.db.get_nick_value(person,'denom'):
-      bot.reply('%s is %s' % (person, bot.db.get_nick_value(person.lower(),'denom')))
+      bot.reply('%s is %s' % (person, bot.db.get_nick_value(person,'denom')))
     else:
       bot.reply('I don\'t know what %s is.' % person)
 
-@sopel.module.event('NOTICE')
-@sopel.module.rule('Information on \x02(.*)\x02 \(account \x02(.*)\x02\):')
-def nickserv_info_account(bot, trigger):
-    person = trigger.nick
-    groups = [ x.lower() for x in trigger.groups() ]
-    if person == 'NickServ' and groups[0] == groups[1]:
-        if groups[0] in bot.memory['denom_ns_pending_checks']:
-            # this person is authenticated to NickServ and on their account, so they proceed to the next level
-            bot.memory['denom_ns_checking'] = groups[0]
-    else:
-        # they failed the requirements
-        if groups[0] in bot.memory['denom_ns_pending_checks']:
-            sender = bot.memory['denom_ns_pending_checks'][groups[0]][0]
-            del bot.memory['denom_ns_pending_checks'][groups[0]]
-            bot.msg(sender, 'Sorry, you need to be authed to services to use this command.')
-
-@sopel.module.event('NOTICE')
-@sopel.module.rule('Last seen  : (.+)')
-def nickserv_info_last_seen(bot, trigger):
-    person = trigger.nick
-    groups = trigger.groups()
-    if person == 'NickServ' and groups[0] == 'now':
-        if bot.memory['denom_ns_checking'] in bot.memory['denom_ns_pending_checks']:
-            # whoever we're currently checking is currently authed
-            nickname = bot.memory['denom_ns_checking']
-            sender, denom = bot.memory['denom_ns_pending_checks'][nickname]
-            bot.db.set_nick_value(nickname, 'denom', denom)
-            del bot.memory['denom_ns_pending_checks'][nickname]
-            bot.memory['denom_ns_checking'] = None
-            bot.msg(sender, 'Got it: %s is %s' % (nickname, denom))
-        else:
-            # this should never happen
-            bot.memory['denom_ns_checking'] = None
-    else:
-        # they failed the requirements
-        if bot.memory['denom_ns_checking'] is not None:
-            sender = bot.memory['denom_ns_pending_checks'][bot.memory['denom_ns_checking']][0]
-            del bot.memory['denom_ns_pending_checks'][bot.memory['denom_ns_checking']]
-            bot.memory['denom_ns_checking'] = None
-            bot.msg(sender, 'Sorry, you need to be authed to services to use this command.')
